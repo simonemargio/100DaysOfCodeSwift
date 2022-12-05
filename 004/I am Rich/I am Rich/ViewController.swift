a2acffe11cb50f17642f014851d79394b0fe9e6d
Simone Margio <simone.margio@icloud.com>
2022-12-05 09:58:30 +0100

    Day 004
    Design user interface and app icon

---
+++ 004/I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Simone Margio.
+//  www.simonemargio.im
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 004/README.md
@@ -0,0 +1,11 @@
+# Day 4
+
+&nbsp;
+
+### Today's topic 🎯
+Design user interface and app icon. We are all rich with a doge as an app! 🐶
+
+&nbsp;
+
+### Final result 🎉
+![04](https://user-images.githubusercontent.com/110282927/181903519-8a6bc7c3-7ef7-4472-958c-cda967f56189.png)
