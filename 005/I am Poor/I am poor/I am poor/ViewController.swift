8cb6fb3a9b837aa5e4c8bf8cb7a19bf04087181f
Simone Margio <simone.margio@icloud.com>
2022-12-06 10:32:12 +0100

    Day 005
    Xcode storyboard and interface builder

---
+++ 005/I am Poor/I am poor/I am poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am poor
+//
+//  Created by Simone Margio.
+//  www.simonemargio.im
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 005/README.md
@@ -0,0 +1,11 @@
+# Day 5
+
+&nbsp;
+
+### Today's topic 🎯
+Xcode storyboard and interface builder challenge. The real richness is how to make cookies! 🍪
+
+&nbsp;
+
+### Final result 🎉
+![05](https://user-images.githubusercontent.com/22590804/205873436-52c2f376-88cd-4a6c-a663-dc5c78fa374e.png)
