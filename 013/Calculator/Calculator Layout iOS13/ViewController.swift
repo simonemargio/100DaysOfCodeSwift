2f1c097b6e9c4d0aec7d2b453807dd76377c5007
Simone Margio <simone.margio@icloud.com>
2022-12-14 12:09:46 +0100

    Day 013
    Calculator auto layout

---
+++ 013/Calculator/Calculator Layout iOS13/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator Layout iOS13
+//
+//  Created by Simone Margio.
+//  www.simonemargio.im
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 013/README.md
@@ -0,0 +1,12 @@
+# Day 13
+
+&nbsp;
+
+### Today's topic 🎯
+Calculator auto layout
+
+&nbsp;
+
+### Final result 🎉
+https://user-images.githubusercontent.com/22590804/207579143-19a0c32d-13c1-41c0-a967-e64f21688135.mov
+
