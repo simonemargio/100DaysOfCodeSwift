5ca45ff3e1b639eb8e7a7d230da3c92b1d79ab87
Simone Margio <simone.margio@icloud.com>
2023-01-28 10:03:04 +0100

    Day 042
    Using extensions to refactor, CoreLocation, and get location data

---
+++ 042/Clima/Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,131 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Simone Margio
+//  www.simonemargio.im
+//
+
+import UIKit
+import CoreLocation
+
+// With UITextFieldDelegate we can control when the user types on keyboard
+class WeatherViewController: UIViewController  {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextFiled: UITextField!
+    
+    var weatherManager = WeatherManager()
+    var locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Before get GPS location we need the user allow
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+      
+        
+        
+        /* The textfield shuold report back to self/WeatherViewController
+            Here the searchTextField tells to WeatherViewController (self) if there is a change in the state of searchTextFiled. Self is the delegate.
+         */
+        weatherManager.delegate = self
+        searchTextFiled.delegate = self
+        
+    }
+
+}
+
+// The extension adopts the WeatherViewController class with the UITextViewDelegate protocol
+extension WeatherViewController: UITextFieldDelegate {
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        // Close the keyboard
+        searchTextFiled.endEditing(true)
+        print(searchTextFiled.text!)
+    }
+    
+    // Ask to delegate if the user presses the keyboard "go" button
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // What to do after "go" button
+        print("Keyboard go button pressed")
+        searchTextFiled.endEditing(true)
+        print(searchTextFiled.text!)
+        return true
+    }
+    
+    // What to do if the users decides to end writing
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        print("User has stopped writing")
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "wft"
+            return false
+        }
+    }
+    
+    // Ask to delegate if the user has end to write
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("End")
+        if let city = textField.text {
+            //  Call the function for create API request and get data
+            weatherManager.fetchWeather(cityName: city)
+        }
+        searchTextFiled.text = ""
+    }
+}
+
+extension WeatherViewController: WeatherManagerDelegate {
+  
+    // Update Weather info
+    func didUpdateWeather(_ weatherMeneger: WeatherManager, weather: WeatherModel) {
+        /*
+        The process of getting weather information over the internet can take time. If we manage the temperature change, weather icon and others from the main process our app will be blocked.
+         It is therefore necessary to use a different process from the main one which, once the weather information has been obtained, allows the main process to change the labels.
+         */
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString
+            self.cityLabel.text = weather.cityName
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+        }
+        
+    }
+    
+    // Menage error by Weather performeRequest or parseJSON
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+}
+
+
+extension WeatherViewController {
+    @IBAction func locationPressed(_ sender: UIButton) {
+        locationManager.requestLocation()
+    }
+}
+
+extension WeatherViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("Got location data")
+        // Obtain last element into the array (last = last coordinates, more accurate)
+        if let location = locations.last {
+            // Stop the location manager, we have all we need
+            locationManager.stopUpdatingLocation()
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            print("Latitude: \(lat)\nLongitude: \(lon)")
+            
+            weatherManager.fetchWeather(latitude: lat, longitude: lon)
+            
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+}
---
+++ 042/README.md
@@ -0,0 +1,11 @@
+# Day 42
+
+&nbsp;
+
+### Today's topic 🎯
+Using extensions to refactor, CoreLocation, and get location data
+
+&nbsp;
+
+### Final result 🎉
+https://user-images.githubusercontent.com/110282927/188493339-60d98942-e7f7-449c-af16-b45a630c9caf.mov
